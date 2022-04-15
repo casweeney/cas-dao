e751785d779a41f521b4b535151742693b497396
CodingCas <casweeno2000@gmail.com>
2022-04-15 10:00:21 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Crypto-Dev-DAO
---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x5C8f5D80FD653a885832dF5c1f09978C81A8cDC4";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
